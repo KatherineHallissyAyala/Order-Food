816b56a3dca9b640410f3bd1c13666414a1cf12b
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2023-11-16 14:38:54 -0500

    restaurantList - strapi URL

--- frontend/components/RestaurantList.js
+++ frontend/components/RestaurantList.js
@@ -33,7 +33,7 @@ function RestaurantCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`${"https://order-6fq5.onrender.com" || "http://127.0.0.1:1337"}${
+          src={`${"https://order-6fq5.onrender.com"}${
             data.attributes.image.data.attributes.url
           }`}
           alt=""
