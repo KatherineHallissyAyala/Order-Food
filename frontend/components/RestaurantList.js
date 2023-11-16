31e18da9d5c8dd95f53d8ee6aadfa2025510a7c3
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2023-11-16 12:42:56 -0500

    restaurantList - strapi URL

--- frontend/components/RestaurantList.js
+++ frontend/components/RestaurantList.js
@@ -33,7 +33,7 @@ function RestaurantCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`${process.env.STRAPI_URL || "http://127.0.0.1:1337"}${
+          src={`${env.STRAPI_URL || "http://127.0.0.1:1337"}${
             data.attributes.image.data.attributes.url
           }`}
           alt=""
